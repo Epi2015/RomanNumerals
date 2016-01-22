f7a2aeff5ff5d1def15a658c61e1d774c39560c6
Sean Burgoyne and veronica alley <student@epicodus.com>
2016-01-21 16:41:17 -0800

    add exception functionality and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ conversion = function(inputInt) {
   var roman = ["M", "C", "X", "I"]
   var indexes = [0, 1, 2, 3]
 
+  // var exceptions = ["CCCCCCCC", "CCCCC", "CCCC", "XXXXXXXXXXXXXX", "XXXXX", "XXXX", "IIIIIIIII", "IIIII", "IIII" ]
+  // var conversionExceptions = ["CM", "D", "CD", "XC", "L", "XL", "IX", "V", "IV"]
+
   var results = []
   if (inputInt <= 3999){
       var output = inputInt.toString().split('').map(function(t){return parseInt(t)});
@@ -14,7 +17,6 @@ conversion = function(inputInt) {
         output.unshift(0);
       }
 
-      debugger;
       indexes.forEach(function(index) {
         var outputIndex = index
 
@@ -25,11 +27,33 @@ conversion = function(inputInt) {
         }
       });
 
-        console.log(results);
+        var stringResults = results.join("");
+        var stringResults = stringResults.replace(/CCCCCCCCC/gi, 'CM');
+        var stringResults = stringResults.replace(/CCCCC/gi, 'D');
+        var stringResults = stringResults.replace(/CCCC/gi, 'CD');
+        var stringResults = stringResults.replace(/XXXXXXXXX/gi, 'XC');
+        var stringResults = stringResults.replace(/XXXXX/gi, 'L');
+        var stringResults = stringResults.replace(/XXXX/gi, 'XL');
+        var stringResults = stringResults.replace(/IIIIIIIII/gi, 'IX');
+        var stringResults = stringResults.replace(/IIIII/gi, 'V');
+        var stringResults = stringResults.replace(/IIII/gi, 'IV');
 
+        return stringResults;
 
   }else {
     return false;
     alert("That's not a compatible number.")
   }
 }
+
+
+$(document).ready(function(){
+  $("#numeralCalculator").submit(function(event){
+    var inputInt =$("#inputInt").val();
+    var finalNumber = conversion(inputInt);
+    // location.href = "puzzleOutput.html";
+    $(".calculated").text(finalNumber);
+
+    event.preventDefault();
+  });
+});
